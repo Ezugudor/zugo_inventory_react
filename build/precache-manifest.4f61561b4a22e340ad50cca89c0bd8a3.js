474c3d263e9302ac1626db62fa5c7aa5f46a8f52
Ossaija ThankGod <codebugsolved@gmail.com>
2018-12-04 02:22:26 +0100

    feat: About to ship
    Porting to the mother ship

---
+++ build/precache-manifest.4f61561b4a22e340ad50cca89c0bd8a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3af4f153abf3ede8c5b21a489ab825e6",
+    "url": "/static/media/signature.3af4f153.svg"
+  },
+  {
+    "revision": "8a99288b26005879d8a8",
+    "url": "/static/css/main.f5738b56.chunk.css"
+  },
+  {
+    "revision": "1f72fe7d412255e44862",
+    "url": "/static/js/1.1f72fe7d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f3d0a86c3d42453a9d62f9cac291bd96",
+    "url": "/static/media/block.f3d0a86c.svg"
+  },
+  {
+    "revision": "8a99288b26005879d8a8",
+    "url": "/static/js/main.8a99288b.chunk.js"
+  },
+  {
+    "revision": "e70d1accd19a4741feb76f28f7b7b2d8",
+    "url": "/static/media/passport.e70d1acc.svg"
+  },
+  {
+    "revision": "862de94b6794fe9666138b8fd97cc889",
+    "url": "/static/media/id-card.862de94b.svg"
+  },
+  {
+    "revision": "e1b86bd1650cd9e4bb6d599679d4afa7",
+    "url": "/static/media/letter.e1b86bd1.svg"
+  },
+  {
+    "revision": "8d564d3625bade44f44c82bcf0d7c480",
+    "url": "/static/media/settings.8d564d36.svg"
+  },
+  {
+    "revision": "63cdf57266321cfdb41587cfeeae2fb4",
+    "url": "/index.html"
+  }
+];
