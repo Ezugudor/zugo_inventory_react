a8e857e3c880bae24aa80155a8f99c89cb6e6fde
ezugudor <nelsonsmrt@gmail.com>
2020-01-23 14:37:32 +0100

    changed url

--- src/core/api/SwypPartners.js
+++ src/core/api/SwypPartners.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const SwypPartnerApi = axios.create({
-  // baseURL: "https://business-backend-service.herokuapp.com/api/v1/"
-  baseURL: "http://localhost:4000/api/v1/"
+  baseURL: "https://business-backend-service.herokuapp.com/api/v1/"
+  // baseURL: "http://localhost:4000/api/v1/"
 });
